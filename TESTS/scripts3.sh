8ee19eae2cfdfdbebf5cf224bc3d942e050424d5
amkharazi <a.m.kharazi.ac@gmail.com>
2025-05-10 09:00:10 +0330

    updated

--- TESTS/scripts3.sh
+++ TESTS/scripts3.sh
@@ -10,15 +10,15 @@
 
 # # FASHION MNIST
 
-python  TEST_basic_timm.py    --TEST_ID TEST_FINAL_ID034    --dataset fashionmnist    --batch_size 32 --n_epoch 200 --image_size 224  --patch_size 4  --num_classes 10    --dim 64    --depth 6   --heads 8   --mlp_dim 128 
-python  TEST_basic_timm_val.py    --TEST_ID TEST_FINAL_ID034    --dataset fashionmnist    --batch_size 32 --n_epoch 200 --image_size 224  --patch_size 4  --num_classes 10    --dim 64    --depth 6   --heads 8   --mlp_dim 128 
+# python  TEST_basic_timm.py    --TEST_ID TEST_FINAL_ID034    --dataset fashionmnist    --batch_size 32 --n_epoch 200 --image_size 224  --patch_size 4  --num_classes 10    --dim 64    --depth 6   --heads 8   --mlp_dim 128 
+# python  TEST_basic_timm_val.py    --TEST_ID TEST_FINAL_ID034    --dataset fashionmnist    --batch_size 32 --n_epoch 200 --image_size 224  --patch_size 4  --num_classes 10    --dim 64    --depth 6   --heads 8   --mlp_dim 128 
 
 # # CIFAR10
 
 # python  TEST_basic_timm.py    --TEST_ID TEST_FINAL_ID035    --dataset cifar10    --batch_size 32 --n_epoch 200 --image_size 224  --patch_size 4  --num_classes 10    --dim 64    --depth 6   --heads 8   --mlp_dim 128 
 # python  TEST_basic_timm_val.py    --TEST_ID TEST_FINAL_ID035    --dataset cifar10    --batch_size 32 --n_epoch 200 --image_size 224  --patch_size 4  --num_classes 10    --dim 64    --depth 6   --heads 8   --mlp_dim 128 
 
-# # STL10
+# # STL10   
 
 # python  TEST_basic_timm.py    --TEST_ID TEST_FINAL_ID036    --dataset stl10   --batch_size 32 --n_epoch 200 --image_size 224  --patch_size 4  --num_classes 10    --dim 64    --depth 6   --heads 8   --mlp_dim 128 
 # python  TEST_basic_timm_val.py    --TEST_ID TEST_FINAL_ID036    --dataset stl10   --batch_size 32 --n_epoch 200 --image_size 224  --patch_size 4  --num_classes 10    --dim 64    --depth 6   --heads 8   --mlp_dim 128 
