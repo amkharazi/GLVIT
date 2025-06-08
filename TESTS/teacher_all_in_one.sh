534adbcfdeffb8c167c8e8e8d922833c49559f8f
Amir Mohammad Kharazi <a.m.kharazi.ac@gmail.com>
2025-06-08 23:47:20 +0330

    updated

---
+++ TESTS/teacher_all_in_one.sh
@@ -0,0 +1,15 @@
+# python teacher_model_all_in_one.py --TEST_ID TEACHER_CIFAR10 --dataset cifar10 --batch_size 256 --n_epoch 100 --image_size 32 --patch_size 4 --num_classes 10 --dim 64 --depth 6 --heads 8 --mlp_dim 128
+
+# python teacher_model_all_in_one --TEST_ID TEACHER_CIFAR100 --dataset cifar100 --batch_size 256 --n_epoch 100 --image_size 32 --patch_size 4 --num_classes 100 --dim 64 --depth 6 --heads 8 --mlp_dim 128
+
+# python teacher_model_all_in_one --TEST_ID TEACHER_MNIST --dataset mnist --batch_size 256 --n_epoch 100 --image_size 32 --patch_size 4 --num_classes 10 --dim 64 --depth 6 --heads 8 --mlp_dim 128
+
+# python teacher_model_all_in_one --TEST_ID TEACHER_FASHIONMNIST --dataset fashionmnist --batch_size 256 --n_epoch 100 --image_size 32 --patch_size 4 --num_classes 10 --dim 64 --depth 6 --heads 8 --mlp_dim 128
+
+# python teacher_model_all_in_one --TEST_ID TEACHER_TINYIMAGENET --dataset tinyimagenet --batch_size 256 --n_epoch 100 --image_size 32 --patch_size 4 --num_classes 200 --dim 64 --depth 6 --heads 8 --mlp_dim 128
+
+# python teacher_model_all_in_one --TEST_ID TEACHER_FLOWERS102 --dataset flowers102 --batch_size 256 --n_epoch 100 --image_size 32 --patch_size 4 --num_classes 102 --dim 64 --depth 6 --heads 8 --mlp_dim 128
+
+# python teacher_model_all_in_one --TEST_ID TEACHER_OXFORDPETS --dataset oxford_pets --batch_size 256 --n_epoch 100 --image_size 32 --patch_size 4 --num_classes 37 --dim 64 --depth 6 --heads 8 --mlp_dim 128
+
+# python teacher_model_all_in_one --TEST_ID TEACHER_STL10 --dataset stl10 --batch_size 256 --n_epoch 100 --image_size 32 --patch_size 4 --num_classes 10 --dim 64 --depth 6 --heads 8 --mlp_dim 128
