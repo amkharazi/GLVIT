c47ca71e1d28b94d14ac8f2dfec20dc3f08eb429
A.M.Kharazi <56943973+amkharazi@users.noreply.github.com>
2025-06-09 14:51:14 +0330

    updated

--- TESTS/teacher_all_in_one.sh
+++ TESTS/teacher_all_in_one.sh
@@ -1,6 +1,6 @@
 # python teacher_model_all_in_one.py --TEST_ID TEACHER_CIFAR10 --dataset cifar10 --batch_size 256 --n_epoch 100 --image_size 224 --patch_size 4 --num_classes 10 --dim 64 --depth 6 --heads 8 --mlp_dim 128
 
-# python teacher_model_all_in_one --TEST_ID TEACHER_CIFAR100 --dataset cifar100 --batch_size 256 --n_epoch 100 --image_size 224 --patch_size 4 --num_classes 100 --dim 64 --depth 6 --heads 8 --mlp_dim 128
+python teacher_model_all_in_one --TEST_ID TEACHER_CIFAR100 --dataset cifar100 --batch_size 256 --n_epoch 100 --image_size 224 --patch_size 4 --num_classes 100 --dim 64 --depth 6 --heads 8 --mlp_dim 128
 
 # python teacher_model_all_in_one --TEST_ID TEACHER_MNIST --dataset mnist --batch_size 256 --n_epoch 100 --image_size 224 --patch_size 4 --num_classes 10 --dim 64 --depth 6 --heads 8 --mlp_dim 128
 
