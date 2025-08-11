40a40e3e7cd7ff6f20b52a1b162702f43bcbafba
amkharazi <a.m.kharazi.ac@gmail.com>
2025-08-11 19:31:55 +0330

    updated

--- TESTS/pretrained-ours.sh
+++ TESTS/pretrained-ours.sh
@@ -1,33 +1,33 @@
 # # # # MNIST
-# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID001    --dataset mnist    --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
-# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID001    --dataset mnist    --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
+# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID001    --dataset mnist    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
+# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID001    --dataset mnist    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
 # sleep 60
 # # # # FASHION MNIST
-# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID002    --dataset fashionmnist    --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
-# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID002    --dataset fashionmnist    --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
+# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID002    --dataset fashionmnist    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
+# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID002    --dataset fashionmnist    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
 # sleep 60
 # # # # CIFAR10
-# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID003    --dataset cifar10    --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
-# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID003    --dataset cifar10    --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
+# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID003    --dataset cifar10    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
+# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID003    --dataset cifar10    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
 # sleep 60
 # # # # STL10
-# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID004    --dataset stl10   --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
-# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID004    --dataset stl10   --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
+# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID004    --dataset stl10   --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
+# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID004    --dataset stl10   --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 10 --attention_method default
 # sleep 60
 # # # # FLOWERS 102
-# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID005    --dataset flowers102    --batch_size 256 --n_epoch 50 --num_classes 102 --attention_method default
-# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID005    --dataset flowers102    --batch_size 256 --n_epoch 50 --num_classes 102 --attention_method default
+# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID005    --dataset flowers102    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 102 --attention_method default
+# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID005    --dataset flowers102    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 102 --attention_method default
 # sleep 60
 # # # # OXFORD PETS
-# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID006    --dataset oxford_pets    --batch_size 256 --n_epoch 50 --num_classes 37 --attention_method default
-# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID006    --dataset oxford_pets    --batch_size 256 --n_epoch 50 --num_classes 37 --attention_method default
+# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID006    --dataset oxford_pets    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 37 --attention_method default
+# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID006    --dataset oxford_pets    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 37 --attention_method default
 # sleep 60
 # # # # CIFAR100
-python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID007    --dataset cifar100    --batch_size 256 --n_epoch 50 --num_classes 100 --attention_method default
+python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID007    --dataset cifar100    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 100 --attention_method default
 sleep 60
-python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID007    --dataset cifar100    --batch_size 256 --n_epoch 50 --num_classes 100 --attention_method default
+python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID007    --dataset cifar100    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 100 --attention_method default
 # sleep 60
 # # # # TINY IMAGENET 200
-# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID008    --dataset tinyimagenet    --batch_size 256 --n_epoch 50 --num_classes 200 --attention_method default
-# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID008    --dataset tinyimagenet    --batch_size 256 --n_epoch 50 --num_classes 200 --attention_method default
+# python  TEST_ours_timm_v3.py    --TEST_ID OURS_PRETRAINED_v1_ID008    --dataset tinyimagenet    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 200 --attention_method default
+# python  TEST_ours_timm_v3_val.py    --TEST_ID OURS_PRETRAINED_v1_ID008    --dataset tinyimagenet    --image_size 224 --batch_size 256 --n_epoch 50 --num_classes 200 --attention_method default
 # sleep 60
